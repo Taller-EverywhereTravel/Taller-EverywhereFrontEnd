f3e17b4e147deac73b1c99f320f1b24bda7dd662
AcevedoDy <dylan.acevedo2843@gmail.com>
2026-05-15 23:53:14 -0500

    adding backend

--- src/environments/enviroment.prod.ts
+++ src/environments/enviroment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production : true,
-    baseURL : "http://localhost:8080/api/v1",
+    baseURL : "https://everywherebackend-ip6h.onrender.com/api/v1",
     useMocks : false
 };
